7462d01a18575953bea0a73bd17a111f220a5dcf
kyj <7saval@gmail.com>
2025-12-10 12:14:13 +0900

    251210 | 프로젝트id로 컨텍스트 버튼 보임 여부 설정

--- frontend/src/components/StoryContextPanel.tsx
+++ frontend/src/components/StoryContextPanel.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { SynopsisModal } from "../pages/modal/SynopsisModal";
 import { LorebookModal } from "../pages/modal/LorebookModal";
+import { Button } from "./ui/button";
 
 export function StoryContextPanel({ projectId }: { projectId: number }) {
     const [isSynopModalOpen, setIsSynopModalOpen] = useState(false);
@@ -12,40 +13,52 @@ export function StoryContextPanel({ projectId }: { projectId: number }) {
                 <header className="mb-4 flex items-center justify-between">
                     <h2 className="text-lg font-semibold text-slate-900">이야기 컨텍스트</h2>
                 </header>
-                {/* 스크롤 가능한 영역 (헤더 제외) */}
-                <div className="flex-1 overflow-y-auto pr-2 custom-scrollbar">
-                    {/* 시놉시스 */}
-                    <section className="space-y-2">
-                        <button
-                            className="w-full text-left text-base p-2 font-medium text-slate-600 hover:bg-slate-100 hover:text-slate-900"
-                            onClick={() => setIsSynopModalOpen(true)}
-                        >
-                            시놉시스
-                        </button>
-                        {/* 설정집 */}
-                        <button
-                            className="w-full text-left text-base p-2 font-medium text-slate-600  hover:bg-slate-100 hover:text-slate-900"
-                            onClick={() => setIsLoreModalOpen(true)}
-                        >
-                            설정집
-                        </button>
-                    </section>
-                </div>
-            </aside>
+                {
+                    projectId ? (
+                        <div className="flex-1 overflow-y-auto pr-2 custom-scrollbar">
+                            {/* 스크롤 가능한 영역 (헤더 제외) */}
+                            {/* 시놉시스 */}
+                            <section className="space-y-2">
+                                <Button
+                                    variant="outline"
+                                    className="w-full"
+                                    onClick={() => setIsSynopModalOpen(true)}
+                                >
+                                    시놉시스
+                                </Button>
+                                {/* 설정집 */}
+                                <Button
+                                    variant="outline"
+                                    className="w-full"
+                                    onClick={() => setIsLoreModalOpen(true)}
+                                >
+                                    설정집
+                                </Button>
+                            </section>
+                        </div>
+                    ) : (
+                        <div></div>
+                    )
+                }
+
+            </aside >
             {isSynopModalOpen && (
                 <SynopsisModal
                     projectId={projectId}
                     open={isSynopModalOpen}
                     onOpenChange={setIsSynopModalOpen}
                 />
-            )}
-            {isLoreModalOpen && (
-                <LorebookModal
-                    projectId={projectId}
-                    open={isLoreModalOpen}
-                    onOpenChange={setIsLoreModalOpen}
-                />
-            )}
+            )
+            }
+            {
+                isLoreModalOpen && (
+                    <LorebookModal
+                        projectId={projectId}
+                        open={isLoreModalOpen}
+                        onOpenChange={setIsLoreModalOpen}
+                    />
+                )
+            }
         </>
     )
 }
