e683191a9dd4048cde80b1e33bd9168fe4f2bf14
kyj <7saval@gmail.com>
2026-06-01 21:08:08 +0900

    feat:Vercel Analytics 설치

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './lib/queryClient'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { inject } from "@vercel/analytics";
 
 const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
-createRoot(document.getElementById('root')!).render(
+inject();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <GoogleOAuthProvider clientId={CLIENT_ID || ""}>
@@ -16,4 +19,4 @@ createRoot(document.getElementById('root')!).render(
       </GoogleOAuthProvider>
     </QueryClientProvider>
   </StrictMode>,
-)
+);
